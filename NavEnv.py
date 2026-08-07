3b03e2a419700c787dc2297d56ad0261e820d92b
Sobhangh <sobhan.gharanfoli@gmail.com>
2026-08-07 17:54:54 +0200

    Reward engineering with probabilistic termination to encourage exploration + high end reward to evaluate learning better

--- NavEnv.py
+++ NavEnv.py
@@ -164,12 +164,13 @@ class GridNavEnv(gym.Env):
 
 		hit_obstacle = self.obstacle_grid[nr, nc] == 1
 		if (nr, nc) == self.goal_pos:
-			reward = 10.0
+			reward = 100.0
 			terminated = True
 		elif hit_obstacle:
-			reward = -5.0
+			reward = -2.0
 			nr, nc = r, c
-			terminated = True
+			if np.random.random() < 0.2:
+				terminated = True
 		self.agent_pos = (nr, nc)
 		self.episode_return += float(reward)
 		self.episode_length += 1

55bac0c8958588801d04a061e83723514f25f891
Sobhangh <sobhan.gharanfoli@gmail.com>
2026-08-07 11:27:16 +0200

    change in rewards to makes sure the game ends when obstacle hit so that game is short

--- NavEnv.py
+++ NavEnv.py
@@ -164,11 +164,12 @@ class GridNavEnv(gym.Env):
 
 		hit_obstacle = self.obstacle_grid[nr, nc] == 1
 		if (nr, nc) == self.goal_pos:
-			reward = 50.0
+			reward = 10.0
 			terminated = True
 		elif hit_obstacle:
-			reward = -20.0
+			reward = -5.0
 			nr, nc = r, c
+			terminated = True
 		self.agent_pos = (nr, nc)
 		self.episode_return += float(reward)
 		self.episode_length += 1
